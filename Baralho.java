1853593b21ed9b08bfdfba89a5115ef3546f5782
lauramesquitabruel <137446250+lauramesquitabruel@users.noreply.github.com>
2023-09-14 15:37:49 -0300

    Add files via upload

--- Baralho.java
+++ Baralho.java
@@ -1,34 +1,37 @@
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
 import java.util.Scanner;
-import Carta;
 
 public class Baralho{
-    List pilhaCartas = new ArrayList();
+    ArrayList<Carta> pilhaCartas = new ArrayList<Carta>();
 
     public Baralho(){
         Scanner sc = new Scanner(System.in);
+        Carta c;
 
         for(int i = 0; i<52; i++){
             System.out.print("Naipe: ");
             String n = sc.next();
+            c.naipe = n;
             System.out.print("Valor: ");
-            char v = sc.next();
-            Carta c = Carta(n, v);
-            pilhaCartas.add(i, c);
+            String v = sc.next();
+            c.valor = v;
+            pilhaCartas.add(c);
         }
     }
 
-    public Baralho embaralhar(){
-       return Collections.shuffle(pilhaCartas);
+    public void embaralhar(){
+        Collections.shuffle(pilhaCartas, null);
     }
 
-    public Carta pescar() throws Exception {
-        if(pilhaCartas.isEmpty() == True ){
+
+    public ArrayList<Carta> pescar() throws Exception {
+        boolean r = pilhaCartas.isEmpty();
+        if(r == true){
             throw new Exception("Baralho vazio, não é possível pescar mais cartas");
+        } else {
+            return pilhaCartas[0];
         }
-        Carta carta_retirada = pilhaCartas[0];
-        return carta_retirada;
+    
     }
 }
--- Main.java
+++ Main.java
@@ -4,13 +4,4 @@ import Baralho;
 import Carta;
 
 public class Main{
-    Baralho baralho;
-    List maoJogador = new ArrayList();
-
-    Baralho();
-    embaralhar();
-    for(int x = 0; x<7; x++){
-        Carta carta_nova = pescar();
-        maoJogador.add(x, carta_nova);
-    }
 }
